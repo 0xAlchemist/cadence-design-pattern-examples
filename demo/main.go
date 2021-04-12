201fd6c80f9a307fd5fff0b53455acf2b3bb49e2
0xAlchemist <eric@ebdigital.ca>
2021-04-12 12:50:42 -0700

    clean up demo script, add comments

--- demo/main.go
+++ demo/main.go
@@ -5,23 +5,33 @@ import (
 )
 
 func main() {
+	// Create a new go-with-the-flow instance
+	// - create the Actor account
+	//
 	g := gwtf.
 		NewGoWithTheFlowEmulator().
 		CreateAccount("actor")
 
 	// create the Actor Resource
+	// - sign as the Actor
 	g.
 		TransactionFromFile("createActorResource").
 		SignProposeAndPayAs("actor").
 		Run()
 
-	// fail running doSomethingCool as the actor
+	// running doSomethingCool as the Actor
+	// would fail here, as the ActorResource
+	// hasn't received the capability yet
+	//
 	// g.
 	// 	TransactionFromFile("doSomethingCool").
 	// 	SignProposeAndPayAs("actor").
 	// 	Run()
 
 	// add capability to capability receiver with admin resource
+	// - pass in the Actor's account argument (Address)
+	// - sign as the Deployer
+	//
 	g.
 		TransactionFromFile("addCapability").
 		AccountArgument("actor").
@@ -29,6 +39,8 @@ func main() {
 		Run()
 
 	// successfully run the doSomethingCool method
+	// - sign as the Actor
+	//
 	g.
 		TransactionFromFile("doSomethingCool").
 		SignProposeAndPayAs("actor").
